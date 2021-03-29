7c18d0807acc92c1c89418b4998e97214923aed5
Ramojus Lapinskas <ramojus.lapinskas@protonmail.com>
2021-03-29 17:55:18 +0300

    add bar height

--- config.h
+++ config.h
@@ -16,6 +16,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 22;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "CaskaydiaCove Nerd Font:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
